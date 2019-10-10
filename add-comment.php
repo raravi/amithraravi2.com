470b6a131d4739e3a4131bf2c26a9331078ede9f
Amith Raravi <amith.raravi@gmail.com>
2019-10-10 13:50:42 +0200

    Upgraded add-comment.php!

--- add-comment.php
+++ add-comment.php
@@ -1,4 +1,5 @@
 <?php
+    $config = include('../config.php');
     header("Content-type: text/plain; charset=utf-8", true, 200); //Response body is text
     if(isset($_POST['replyingto'])) {
 
@@ -12,7 +13,7 @@
         $slug       = $_POST['slug'];
 
         $token       = $_POST['token'];
-        $secretKey = "6Le7grwUAAAAAPpp3bSKRUfr6-iXTjhnF9_I1TbN";
+        $secretKey = $config['secretKey'];
 
         // post request to server
         $recaptchaurl =  'https://www.google.com/recaptcha/api/siteverify?secret=' . urlencode($secretKey) . '&response=' . urlencode($token);
@@ -20,48 +21,54 @@
         $responseKeys = json_decode($response,true);
         // end post request
 
-        $date = new DateTime();
-        $datetostring = $date->getTimestamp();
-        $myfilename = "entry$datetostring.yml";
-        $myfile = fopen($myfilename, "w") or die("Unable to open file!");
-        $txt = "_id: $datetostring\n";
-        fwrite($myfile, $txt);
-        $txt = "replying_to: '$replyingto'\n";
-        fwrite($myfile, $txt);
-        $txt = "message: \"$message\"\n";
-        fwrite($myfile, $txt);
-        $txt = "name: $name\n";
-        fwrite($myfile, $txt);
-        $txt = "email: $email\n";
-        fwrite($myfile, $txt);
-        $txt = "emailorig: $emailorig\n";
-        fwrite($myfile, $txt);
-        $txt = "url: $url\n";
-        fwrite($myfile, $txt);
-        $txt = "date: $datetostring\n";
-        fwrite($myfile, $txt);
-        $txt = "slug: $slug\n";
-        fwrite($myfile, $txt);
+        //check if the test was done OK, if the action name is correct
+        if ($responseKeys["success"] && $responseKeys["action"] == 'addcomment') {
+          $date = new DateTime();
+          $datetostring = $date->getTimestamp();
+          $myfilename = "entry$datetostring.yml";
+          $myfile = fopen($myfilename, "w") or die("Unable to open file!");
+          $txt = "_id: $datetostring\n";
+          fwrite($myfile, $txt);
+          $txt = "replying_to: '$replyingto'\n";
+          fwrite($myfile, $txt);
+          $txt = "message: \"$message\"\n";
+          fwrite($myfile, $txt);
+          $txt = "name: $name\n";
+          fwrite($myfile, $txt);
+          $txt = "email: $email\n";
+          fwrite($myfile, $txt);
+          $txt = "emailorig: $emailorig\n";
+          fwrite($myfile, $txt);
+          $txt = "url: $url\n";
+          fwrite($myfile, $txt);
+          $txt = "date: $datetostring\n";
+          fwrite($myfile, $txt);
+          $txt = "slug: $slug\n";
+          fwrite($myfile, $txt);
 
-        $responseKeysSuccess = $responseKeys["success"];
-        $txt = "reCaptchaSuccess: $responseKeysSuccess\n";
-        fwrite($myfile, $txt);
-        $responseKeysScore = $responseKeys["score"];
-        $txt = "reCaptchaScore: $responseKeysScore\n";
-        fwrite($myfile, $txt);
-        $responseKeysAction = $responseKeys["action"];
-        $txt = "reCaptchaAction: $responseKeysAction\n";
-        fwrite($myfile, $txt);
-        $responseKeysTS = $responseKeys["challenge_ts"];
-        $txt = "reCaptchaTimestamp: $responseKeysTS\n";
-        fwrite($myfile, $txt);
-        $responseKeysHost = $responseKeys["hostname"];
-        $txt = "reCaptchaHost: $responseKeysHost\n";
-        fwrite($myfile, $txt);
+          $responseKeysSuccess = $responseKeys["success"];
+          $txt = "reCaptchaSuccess: $responseKeysSuccess\n";
+          fwrite($myfile, $txt);
+          $responseKeysScore = $responseKeys["score"];
+          $txt = "reCaptchaScore: $responseKeysScore\n";
+          fwrite($myfile, $txt);
+          $responseKeysAction = $responseKeys["action"];
+          $txt = "reCaptchaAction: $responseKeysAction\n";
+          fwrite($myfile, $txt);
+          $responseKeysTS = $responseKeys["challenge_ts"];
+          $txt = "reCaptchaTimestamp: $responseKeysTS\n";
+          fwrite($myfile, $txt);
+          $responseKeysHost = $responseKeys["hostname"];
+          $txt = "reCaptchaHost: $responseKeysHost\n";
+          fwrite($myfile, $txt);
 
-        fclose($myfile);
+          fclose($myfile);
 
-        exit("OK");
+          exit("OK");
+        }
+        else {
+          exit("The only true wisdom is in knowing you know nothing.");
+        }
     }
     else {
         exit("INVALID REQUEST.");
